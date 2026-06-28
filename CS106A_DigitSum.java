b9b6c073e076a4fd630d9f157c3ee0409606ebe4
Noah Pope <npope@tutanota.com>
2026-06-28 12:57:01 -0500

    adding files to new repo

---
+++ CS106A_DigitSum.java
@@ -0,0 +1,38 @@
+/**
+ * File: CS106A_DigitSum.java
+ * ----------------------------
+ * This Java program sums the digits in a positive integer.
+ * The program depends on the fact that the last digit of 
+ * an integer n is given by n % 10 and the number consisting
+ * of all but the last digit is given by the expression n / 10.
+ */
+
+import acm.program.*;
+
+public class CS106A_DigitSum extends ConsoleProgram
+{	
+	public void run()
+	{ 
+		getIntegerFromUser();
+	}
+	
+	
+	private void getIntegerFromUser()
+	{
+		int userInt = readInt("enter a number you'd like to sum: ");
+		sumDigits(userInt);
+	}
+	
+	
+	private void sumDigits(int userInput)
+	{
+		int dSum = 0;
+		while (userInput > 0){
+			int tmpLastDigit = userInput % 10;
+			userInput /= 10;
+			dSum += tmpLastDigit;
+		}	
+		
+		println("total = " + dSum);
+	}
+}
